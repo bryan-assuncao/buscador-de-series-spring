ed1cf3d603090deb2b939c083afa98c56712532d
Bryan Conceição de Assunçao <bryan.assuncao@safeweb.com.br>
2025-10-20 18:08:17 -0300

    dados temporadas

--- src/main/java/br/com/bryan/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/bryan/screenmatch/ScreenmatchApplication.java
@@ -2,11 +2,15 @@ package br.com.bryan.screenmatch;
 
 import br.com.bryan.screenmatch.model.DadosEpisodio;
 import br.com.bryan.screenmatch.model.DadosSerie;
+import br.com.bryan.screenmatch.model.DadosTemporada;
 import br.com.bryan.screenmatch.service.ConsumoAPI;
 import br.com.bryan.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -28,8 +32,22 @@ public class ScreenmatchApplication implements CommandLineRunner {
         DadosSerie dadosSerie = converteDados.obterDados(json, DadosSerie.class);
         System.out.println(dadosSerie);
 
+        System.out.println("----------------------------------------------------------------------");
+
         json = consumoAPI.obterDados("http://www.omdbapi.com/?t=the+office&season=7&episode=11&apikey=27205e74");
         DadosEpisodio dadosEpisodio = converteDados.obterDados(json, DadosEpisodio.class);
         System.out.println(dadosEpisodio);
-	}
+
+        System.out.println("----------------------------------------------------------------------");
+
+        List<DadosTemporada> temporadas = new ArrayList<>();
+        for (int i = 1; i<= dadosSerie.totalTemporadas(); i++){
+            json = consumoAPI.obterDados("http://www.omdbapi.com/?t=the+office&season=" + i + "&apikey=27205e74");
+            DadosTemporada dadosTemporada = converteDados.obterDados(json, DadosTemporada.class);
+            temporadas.add(dadosTemporada);
+        }
+        temporadas.forEach(System.out::println);
+
+
+    }
 }
